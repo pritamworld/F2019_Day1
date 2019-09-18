642daff9c0bdc78855f45ffa4eaf92199943e05b
moxDroid Labs Inc <pritam.world@gmail.com>
2019-09-18 19:16:42 -0400

    Initial commit

---
+++ src/com/lambton/FirstDay.java
@@ -0,0 +1,19 @@
+package com.lambton;
+
+public class FirstDay
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Java Programming");
+
+        int a, b, c;
+
+        a = 10;
+        b = 20;
+
+        c = a + b;
+
+        System.out.println("SUM : " + c);
+
+    }
+}
